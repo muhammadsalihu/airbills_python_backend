f203d851124dc0b753de6ec70201d44bc691a1a4
Muhammad Salihu <muhammad.salihu1@yahoo.com>
2025-01-31 15:00:31 +0100

    fix: added middleware to fix CORS issues

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# Allow all origins (you can replace '*' with specific domains if needed)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify ['http://localhost:8081']
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Airbills Agent"}
