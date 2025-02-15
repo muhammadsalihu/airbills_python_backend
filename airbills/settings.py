462805db323e8c7ff7982942a89dd3ce86917c02
Muhammad Salihu <muhammad.salihu1@yahoo.com>
2025-02-15 12:12:53 +0100

    update: added deployed azure site to allowed hosts

--- airbills/settings.py
+++ airbills/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'your-secret-key-here'
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["airbills-python-backend.onrender.com", "localhost", "127.0.0.1", "airbills-backend.azurewebsites.net"]
+ALLOWED_HOSTS = ["airbills-python-backend.onrender.com", "localhost", "127.0.0.1", "https://airbills.azurewebsites.net/"]
 
 # Application definition
 
