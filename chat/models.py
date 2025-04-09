57ea13b933541ff091e528266d1fa5df6f656704
Muhammad Salihu <muhammad.salihu1@yahoo.com>
2025-04-09 15:46:05 +0100

    Reset project to clean FastAPI setup

--- chat/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class ChatResponse(models.Model):
-    content = models.TextField()
-    category = models.CharField(max_length=100)
-    has_artifact = models.BooleanField(default=False)
-    artifact_type = models.CharField(max_length=50, null=True, blank=True)
-    artifact_content = models.TextField(null=True, blank=True)
--- startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
